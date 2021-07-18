f768d8c3965e03744e9d0deafe3583be068e2924
BarbodSadra <barboad1380@gmail.com>
2021-07-17 20:51:00 -0700

    Fourth Commit

--- View_Board.cpp
+++ View_Board.cpp
@@ -1,30 +1,162 @@
 #include <iostream>
+#include <iomanip>
+#include <stdio.h>
 
 using namespace std;
 
+
+enum Direction { STOP = 0, UP, DOWN, LEFT, RIGHT };
+Direction dir;
+
+bool GameOver;
+int x = 2;
+int y = 2;
+
+
+void Setup()
+{
+    GameOver = false;
+    x = 2;
+    y = 2;
+}
+
+
 void view()
 {
-    for( int row = 0; row < 34; row++ )
+    for( int row = 0; row < 45; row++ )
     {
-        if( row % 3 == 0 )
+        if( row % 4 == 0 )
         {
-            for( int column = 0; column < 34; column++ )
+            for( int column = 0; column < 45; column++ )
             {
-                cout << 'e' << " ";
+                cout << '*' << " ";
             }
+            cout << endl;
         }
         else
         {
-            for( int column = 0; column <= 44; column += 4 )
+            for( int column = 0; column <= 88; column++ )
             {
-                cout << 'e' << "     ";
+                if( column % 8 == 0 )
+                {
+                    cout << '*';
+                }
+                else
+                {
+                    if( row == x && column == 2*y )
+                    {
+                        cout << 'M';
+                    }
+
+                    else
+                    {
+                        cout << ' ';
+                    }
+                }
             }
+            cout << endl;
         }
-        cout << endl;
     }
 }
 
+
+void Input()
+{
+    switch( getchar() )
+    {
+        case 'w':
+        {
+            dir = UP;
+            break;
+        }
+
+        case 'a':
+        {
+            dir = LEFT;
+            break;
+        }
+
+        case 'd':
+        {
+            dir = RIGHT;
+            break;
+        }
+
+        case 's':
+        {
+            dir = DOWN;
+            break;
+        }
+    }
+}
+
+
+void Logic()
+{
+    switch( dir )
+    {
+        case UP:
+        {
+            x -= 2;
+            cout << "X IS  " << x << endl;
+            break;
+        }
+
+        case LEFT:
+        {
+            y -= 2;
+            cout << "Y IS  " << y << endl;
+            break;
+        }
+
+        case RIGHT:
+        {
+            y += 2;
+            cout << "Y IS  " << y << endl;
+            break;
+        }
+
+        case DOWN:
+        {
+            cout << "X Before Is  " << x << endl;
+            x += 2;
+            cout << "X After IS  " << x << endl;
+            break;
+        }
+    }
+}
+
+
 int main()
 {
-    view();
+    for( int counter = 0; counter < 8; counter++ )
+    {
+        view();
+        Input();
+        Logic();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
